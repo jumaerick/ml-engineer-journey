59162a71fc424ff8f7db85e90d64a0b5f14ded4e
Juma Erick <jumaaerick@gmail.com>
2026-07-21 22:12:24 +0300

    datatypes and string operations

---
+++ Python/Strings.py
@@ -0,0 +1,32 @@
+#Sequence of chars enclosed with single or double quotes
+
+#Escaping double or single quotation marks
+msg = 'It\'s a sunny day'
+
+#Immutable datatypes cannot be modified once declared
+
+name = 'erick juma'
+name[:8:2] #step
+
+def stringMethods(name):
+    """
+    This function will cover basic string operations
+    """
+    print('Lower', ':', name.lower(), '\n')
+    print('Upper', ':', name.upper(), '\n')
+    print('Is upper', ':', name.isupper(), '\n')
+    print('Is lower', ':', name.islower(), '\n')
+    print('Capitalize', ':', name.capitalize(), '\n')
+    print('Title', ':', name.title(), '\n')
+    print('Count occurence', 'j', ':', name.count('j'), '\n')
+    print('Count start of substring', 'ju', ':', name.find('ju'), '\n')
+    print('Strip start and end space', ':', ' juma '.strip(), '\n')
+    print('Split string', ':', 'ju, ma'.split(','), '\n')
+    print('Join erick juma', ':', ','.join([i for i in name.split()]), '\n')
+    print('Name startswith', ':', name.startswith('e'), '\n')
+    print('Name endswith', ':', name.endswith('e'), '\n')
+    print('Contains substring erick', ':', 'erick' in name, '\n')
+    print('Skip every second element', ':', name[::2], '\n')
+    print('Skip every second element starting from the end', ':', name[-1:-4:-2], '\n')
+
+stringMethods('erick juma')
---
+++ Python/pythonBasics.py
@@ -0,0 +1,45 @@
+#Data Type
+"""
+A classification that defines what values an entity can hold and operations that can be performed on it.
+"""
+#Variables
+"""
+Labels for storing and referencing data of various data types
+"""
+#Variable Rules
+"""
+Can't start with a number
+Must only be alphanumerics
+Are case sensitive
+Can't be reserved keywords
+"""
+
+#Common Datatypes
+"""
+String
+Int (32, 64)
+Float(32, 64)
+Decimal
+Dictionary
+List
+Tuple
+Boolean
+Set
+Range
+None
+"""
+
+#Python is dynamicall typed language
+#You don't have to explicitly define the datatype
+# isinstance('needle', 'haystack')  check membership can check for multple using (haystack1, haystack2)
+# type returns datatype a value belongs
+
+#Function
+"""
+A reusable block of code that can be called/invoked to run its code and can accept arguments.
+"""
+
+# A method
+"""
+A function that belongs to a specific class or object
+"""
